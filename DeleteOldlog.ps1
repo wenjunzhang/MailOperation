9e39dc6864d9d7a1a34d7b481fb29aea76d35e09
zhang wenjun <893078000@qq.com>
2016-03-09 16:08:02 +0800

    Revert "Revert "中文注释乱码，改为英文""
    This reverts commit c6855cbd71d584e8ed567f3e7b2ac325fd3ee1d5.

--- DeleteOldlog.ps1
+++ DeleteOldlog.ps1
@@ -1,3 +1,3 @@
 $date = (Get-Date).AddMonths(-2)
-#去掉 -whatif开始真正执行 
+#delete  -whatif and then begin the real excution 
 Get-ChildItem -Path "c:\scripts\E*.log" | where {!$_.PSIsContainer} | foreach {if ($_.LastWriteTime -lt $date){Remove-Item $_ -whatif}}
